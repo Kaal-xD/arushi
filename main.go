c2c82675ea34a65fb51a84d65f5606dc617bc548
Kaal-xD <119674589+Kaal-xD@users.noreply.github.com>
2025-12-11 13:43:51 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -9,7 +10,7 @@ import (
 
 func main() {
 	pref := telebot.Settings{
-		Token:  "2027709241:AAEcqro-6YUIMLk0ip11cnRnM-g-oGVIxd8",
+		Token:  "YOUR_NEW_BOT_TOKEN_HERE", // <- replace with fresh token
 		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
 	}
 
@@ -18,9 +19,37 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// /start command
 	bot.Handle("/start", func(c telebot.Context) error {
-		return c.Send("Hello, your Go bot is running on Ubuntu 22.04 🚀")
+		return c.Send("👋 Hello! Welcome to Arushi Bot.\nType /help to see all commands.")
 	})
 
+	// /help command
+	bot.Handle("/help", func(c telebot.Context) error {
+		text := "📘 *Available Commands:*\n\n" +
+			"/start - Start the bot\n" +
+			"/help - Show this help menu\n" +
+			"/ping - Check bot status\n"
+
+		return c.Send(text, &telebot.SendOptions{ParseMode: telebot.ModeMarkdown})
+	})
+
+	// /ping command
+	bot.Handle("/ping", func(c telebot.Context) error {
+		return c.Send("🏓 Pong! Bot is alive.")
+	})
+
+	// Reply to any text message
+	bot.Handle(telebot.OnText, func(c telebot.Context) error {
+		user := c.Sender().FirstName
+		msg := c.Text()
+
+		reply := "You said: " + msg + "\nNice to meet you, " + user + " 😊"
+
+		return c.Send(reply)
+	})
+
+	log.Println("Bot is running...")
 	bot.Start()
 }
+
