df5633a6c372d5d9adffe38a77266363b6bcc8fe
Kaal-xD <119674589+Kaal-xD@users.noreply.github.com>
2025-12-11 13:46:02 +0530

    Create config.go

---
+++ config.go
@@ -0,0 +1,11 @@
+package main
+
+// Global configuration variables
+
+var (
+	BotToken     = "YOUR_BOT_TOKEN"      // bot token
+	Channel      = "@yourchannel"        // main channel
+	Support      = "@yoursupport"        // support group/user
+	OwnerID int64 = 1234567890           // owner telegram ID
+)
+
