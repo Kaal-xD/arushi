4d39ef78b9105eb44a48e6b038e407f1115d7f25
Kaal-xD <119674589+Kaal-xD@users.noreply.github.com>
2025-12-11 14:01:19 +0530

    Update config.go

--- config.go
+++ config.go
@@ -1,11 +1,11 @@
 package main
 
 // Global configuration variables
-
 var (
-	BotToken     = "2027709241:AAEcqro-6YUIMLk0ip11cnRnM-g-oGVIxd8"      // bot token
-	Channel      = "https://t.me/adityaserver"        // main channel
-	Support      = "https://t.me/adityadiscus"        // support group/user
-	OwnerID int64 = 1282754256           // owner telegram ID
-)
+	BotToken = "2027709241:AAEcqro-6YUIMLk0ip1lcnRnM-g-oGVIxd8" // bot token
 
+	Channel = "https://t.me/adityaserver"     // main channel
+	Support = "https://t.me/adityadiscus"     // support group/user
+
+	OwnerID int64 = 1282754256 // owner telegram ID
+)
